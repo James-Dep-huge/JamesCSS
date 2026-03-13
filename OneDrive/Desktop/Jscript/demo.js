622740c0c5c733a8da9b22c0b7e61b0e57c9c273
James-Dep-huge <jeevagojo2003@gmail.com>
2026-03-13 21:17:18 +0530

    new added

---
+++ OneDrive/Desktop/Jscript/demo.js
@@ -0,0 +1,13 @@
+var factor="Kamal"
+var fplayer="Dhoni"
+var fmovie="Vikrem"
+ function favourite()
+ {
+
+  
+   console.log("Favourite actor:"+"Kamal")
+    console.log("Favourite player:"+"Dhoni")
+    console.log("Favourite movie:"+"Vikram")
+    
+ }
+favourite()
