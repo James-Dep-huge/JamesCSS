903b88c02dfe3e0dd9d4ca96f1bdda192329a6a0
James-Dep-huge <jeevagojo2003@gmail.com>
2026-03-16 20:32:15 +0530

    new added

---
+++ OneDrive/Desktop/HTML tutorial/Jscript/demo.js
@@ -0,0 +1,19 @@
+var season ="winter"
+
+if( season=="spring")
+{
+  console.log("Enjoy blooming flowers")
+}
+if( season=="summer")
+{
+  console.log("Have fun in the sun")
+
+}
+if(season=="autumn")
+{
+  console.log("Admire the colors leaves")
+}
+if(season=="winter")
+{
+  console.log("Bundle up stay warm")
+}
